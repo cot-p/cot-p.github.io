56f040da9c533d737f7b6c253b172b0d3c39e242
alexmercerind <saini123hitesh@gmail.com>
2020-11-25 13:33:59 +0530

    Added separate file to define custom time-table

---
+++ lectures.js
@@ -0,0 +1,43 @@
+//   📖 README
+//   Call [setTitle] method here to set the time-table title.
+//   Call [addLecture] methods here to add lectures into the time-table grid.
+//   Call [addTime] methods here to add lecture timings into the time-table grid.
+
+
+//   ✅ Set time-table title.
+setTitle('Electronics & Communication')
+
+//   ✅ Add lecture timings.
+addTime('9:00 to 10:00');
+addTime('10:00 to 11:00');
+addTime('11:00 to 12:00');
+addTime('12:00 to 13:00');
+addTime('13:00 to 14:00');
+addTime('14:00 to 15:00');
+addTime('15:00 to 16:00');
+addTime('16:00 to 17:00');
+addTime('17:00 to 18:00');
+
+//   ✅ Add various lectures into the time-table grid.
+addLecture('English', 1, 2, 1);
+addLecture('Calculus & Differential Equations', 1, 3, 1);
+addLecture('Workshop Practices', 1, 4, 1);
+addLecture('Semiconductor Physics', 1, 5, 1);
+addLecture('English', 1, 6, 1);
+addLecture('Calculus & Differential Equations', 2, 1, 1);
+addLecture('Calculus & Differential Equations', 2, 2, 1);
+addLecture('Semiconductor Physics', 2, 3, 1);
+addLecture('Engineering Graphics & Design', 2, 4, 1);
+addLecture('Calculus & Differential Equations', 2, 6, 1);
+addLecture('Engineering Graphics & Design', 3, 1, 2);
+addLecture('Calculus & Differential Equations', 3, 2, 2);
+addLecture('Semiconductor Physics Tutorial', 3, 3, 1);
+addLecture('Semiconductor Physics', 3, 6, 1);
+addLecture('Calculus & Differential Equations Tutorial', 4, 3, 1);
+addLecture('Semiconductor Physics Practical', 7, 2, 2);
+addLecture('Workshop Practices', 7, 3, 2);
+addLecture('English Practical', 7, 4, 2);
+addLecture('Workshop Practices', 7, 5, 2);
+addLecture('Environmental Engineering And Disasters', 9, 3, 1);
+addLecture('Environmental Engineering And Disasters', 9, 4, 1);
+addLecture('Environmental Engineering And Disasters', 9, 5, 1);
