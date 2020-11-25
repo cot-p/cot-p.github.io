051fe6ec05d99a24e77ddf8614cb2224266b7fcd
alexmercerind <saini123hitesh@gmail.com>
2020-11-25 13:34:41 +0530

    Organised structure & code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+///   Internal JavaScript.
+///   See lectures.js for more information.
+
 let main = document.getElementById('main');
 
 function addTile(content, x, y, xSpan, type = 'class') {
@@ -42,10 +45,8 @@ function addTile(content, x, y, xSpan, type = 'class') {
         tile.style.left = `${(128 + margin) * x}px`;
         tile.innerHTML = String(content);
         main.appendChild(tile);
-    }
-}
-
-addTile('Electronics & Communication', 0, 0, 1, 'head');
+    };
+};
 
 addTile('Monday', 0, 1, 1, 'day');
 addTile('Tuesday', 0, 2, 1, 'day');
@@ -54,40 +55,16 @@ addTile('Thursday', 0, 4, 1, 'day');
 addTile('Friday', 0, 5, 1, 'day');
 addTile('Saturday', 0, 6, 1, 'day');
 
-addTile('8:00 to 9:00', 1, 0, 1, 'time');
-addTile('9:00 to 10:00', 2, 0, 1, 'time');
-addTile('10:00 to 11:00', 3, 0, 1, 'time');
-addTile('11:00 to 12:00', 4, 0, 1, 'time');
-addTile('12:00 to 13:00', 5, 0, 1, 'time');
-addTile('13:00 to 14:00', 6, 0, 1, 'time');
-addTile('14:00 to 15:00', 7, 0, 1, 'time');
-addTile('15:00 to 16:00', 8, 0, 1, 'time');
-addTile('16:00 to 17:00', 9, 0, 1, 'time');
+let timeIndex = 0;
 
-addTile('Environmental Engineering And Disasters', 1, 3, 1 , 'class');
-addTile('Environmental Engineering And Disasters', 1, 4, 1 , 'class');
-addTile('Environmental Engineering And Disasters', 1, 5, 1 , 'class');
+let addTime = (time) => {
+    timeIndex++;
+    addTile(time, timeIndex, 0, 1, 'time');
+}
 
-addTile('English', 2, 2, 1 , 'class');
-addTile('Calculus & Differential Equations', 2, 3, 1 , 'class');
-addTile('Workshop Practices', 2, 4, 1 , 'class');
-addTile('Semiconductor Physics', 2, 5, 1 , 'class');
-addTile('English', 2, 6, 1 , 'class');
+let setTitle = (title) => {
+    addTile(String(title), 0, 0, 1, 'head');
+    document.getElementById('title').innerHTML = String(title);
+};
+let addLecture = (lectureName, weekDay, lectureNumber, lectureSpan) => addTile(lectureName, weekDay, lectureNumber, lectureSpan, 'class');
 
-addTile('Calculus & Differential Equations', 3, 1, 1 , 'class');
-addTile('Calculus & Differential Equations', 3, 2, 1 , 'class');
-addTile('Semiconductor Physics Tutorial', 3, 3, 1 , 'class');
-addTile('Engineering Graphics & Design', 3, 4, 1 , 'class');
-addTile('Calculus & Differential Equations', 3, 6, 1 , 'class');
-
-addTile('Calculus & Differential Equations', 4, 1, 2, 'class');
-addTile('Calculus & Differential Equations', 4, 2, 2, 'class');
-addTile('Semiconductor Physics Tutorial', 4, 3, 1 , 'class');
-addTile('Semiconductor Physics', 4, 6, 1 , 'class');
-
-addTile('Calculus & Differential Equations Tutorial', 5, 3, 1 , 'class');
-
-addTile('Semiconductor Physics Practical', 8, 2, 2, 'class');
-addTile('Workshop Practices', 8, 3, 2, 'class');
-addTile('English Practical', 8, 4, 2, 'class');
-addTile('Workshop Practices', 8, 5, 2, 'class');
