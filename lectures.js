177b1ce57dfd9dd12be5137f598a937f8854d40d
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-12-16 09:09:23 +0530

    Update lectures.js

--- lectures.js
+++ lectures.js
@@ -8,6 +8,7 @@
 setTitle('Electronics & Communication')
 
 //   ✅ Add lecture timings.
+addTime('8:00 to 9:00');
 addTime('9:00 to 10:00');
 addTime('10:00 to 11:00');
 addTime('11:00 to 12:00');
@@ -19,25 +20,26 @@ addTime('16:00 to 17:00');
 addTime('17:00 to 18:00');
 
 //   ✅ Add various lectures into the time-table grid.
-addLecture('English', 1, 2, 1);
 addLecture('Calculus & Differential Equations', 1, 3, 1);
-addLecture('Workshop Practices', 1, 4, 1);
-addLecture('Semiconductor Physics', 1, 5, 1);
-addLecture('English', 1, 6, 1);
-addLecture('Calculus & Differential Equations', 2, 1, 1);
-addLecture('Calculus & Differential Equations', 2, 2, 1);
-addLecture('Semiconductor Physics', 2, 3, 1);
-addLecture('Engineering Graphics & Design', 2, 4, 1);
-addLecture('Calculus & Differential Equations', 2, 6, 1);
-addLecture('Engineering Graphics & Design', 3, 1, 2);
-addLecture('Engineering Graphics & Design', 3, 2, 2);
-addLecture('Semiconductor Physics Tutorial', 3, 3, 1);
-addLecture('Semiconductor Physics', 3, 6, 1);
-addLecture('Calculus & Differential Equations Tutorial', 4, 3, 1);
-addLecture('Semiconductor Physics Practical', 7, 2, 2);
-addLecture('Workshop Practices', 7, 3, 2);
-addLecture('English Practical', 7, 4, 2);
-addLecture('Workshop Practices', 7, 5, 2);
-addLecture('Environmental Engineering And Disasters', 9, 3, 1);
-addLecture('Environmental Engineering And Disasters', 9, 4, 1);
-addLecture('Environmental Engineering And Disasters', 9, 5, 1);
+addLecture('English', 2, 2, 1);
+addLecture('Calculus & Differential Equations', 2, 3, 1);
+addLecture('Workshop Practices', 2, 4, 1);
+addLecture('Semiconductor Physics', 2, 5, 1);
+addLecture('English', 2, 6, 1);
+addLecture('Calculus & Differential Equations', 3, 1, 1);
+addLecture('Calculus & Differential Equations', 3, 2, 1);
+addLecture('Semiconductor Physics', 3, 3, 1);
+addLecture('Engineering Graphics & Design', 3, 4, 1);
+addLecture('Calculus & Differential Equations', 3, 6, 1);
+addLecture('Engineering Graphics & Design', 4, 1, 2);
+addLecture('Engineering Graphics & Design', 4, 2, 2);
+addLecture('Semiconductor Physics Tutorial', 4, 3, 1);
+addLecture('Semiconductor Physics', 4, 6, 1);
+addLecture('Calculus & Differential Equations Tutorial', 5, 3, 1);
+addLecture('Semiconductor Physics Practical', 8, 2, 2);
+addLecture('Workshop Practices', 8, 3, 2);
+addLecture('English Practical', 8, 4, 2);
+addLecture('Workshop Practices', 8, 5, 2);
+addLecture('Environmental Engineering And Disasters', 10, 3, 1);
+addLecture('Environmental Engineering And Disasters', 10, 4, 1);
+addLecture('Environmental Engineering And Disasters', 10, 5, 1);
