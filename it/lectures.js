f996859c4f5a2b0c0b2362c69a2d28da174dbb4b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-04-19 11:56:46 +0530

    Update lectures.js

--- it/lectures.js
+++ it/lectures.js
@@ -1,6 +1,5 @@
 setTitle('Information Technology')
 
-addTime('8:00 to 9:00');
 addTime('9:00 to 10:00');
 addTime('10:00 to 11:00');
 addTime('11:00 to 12:00');
@@ -11,29 +10,5 @@ addTime('15:00 to 16:00');
 addTime('16:00 to 17:00');
 addTime('17:00 to 18:00');
 
-addLecture('Environmental Engineering And Disasters', 1, 1, 1)
-addLecture('Chemistry', 2, 1, 1)
-addLecture('Programming For Problem Solving', 3, 1, 1)
-addLecture('Basic Electrical Engineering (Tutorial)', 4, 1, 1)
-addLecture('Calculus & Linear Algebra', 5, 1, 1)
-
-addLecture('Chemistry', 2, 2, 1)
-addLecture('Basic Electrical Engineering', 3, 2, 1)
-addLecture('Programming For Problem Solving (Lab)', 4, 2, 2)
-addLecture('Work Program ', 7, 2, 3)
-
-addLecture('Calculus & Linear Algebra', 2, 3, 1)
-addLecture('Basic Electrical Engineering', 3, 3, 1)
-addLecture('Calculus & Linear Algebra (Tutorial)', 4, 3, 1)
-addLecture('Chemistry (Lab)', 7, 3, 3)
-
-addLecture('Chemistry', 1, 4, 1)
-addLecture('Programming For Problem Solving', 2, 4, 1)
-addLecture('Calculus & Linear Algebra', 5, 4, 1)
-addLecture('Basic Electrical Engineering (Lab)', 8, 4, 2)
-
-addLecture('Environmental Engineering And Disasters', 1, 5, 1)
-addLecture('Programming For Problem Solving', 2, 5, 1)
-
-addLecture('Environmental Engineering And Disasters', 1, 6, 1)
-addLecture('Basic Electrical Engineering', 2, 6, 1)
+addLecture('Waves, Optics & Quantum Mechanics', 1, 1, 1);
+addLecture('Multivariable Calculus', 2, 1, 1);
