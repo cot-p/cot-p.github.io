f6fef8544ad4d0db50b13d39f75d92806c67497a
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-04-19 12:11:05 +0530

    Added more lecture tile calls

--- it/lectures.js
+++ it/lectures.js
@@ -1,5 +1,4 @@
 setTitle('Information Technology')
-
 addTime('9:00 to 10:00');
 addTime('10:00 to 11:00');
 addTime('11:00 to 12:00');
@@ -9,8 +8,22 @@ addTime('14:00 to 15:00');
 addTime('15:00 to 16:00');
 addTime('16:00 to 17:00');
 addTime('17:00 to 18:00');
-
+// Monday
 addLecture('Waves, Optics & Quantum Mechanics', 1, 1, 1);
 addLecture('Multivariable Calculus', 2, 1, 1);
 addLecture('Engineering Graphics and Design', 3, 1, 1);
 addLecture('Engineering Graphics and Design', 7, 1, 2);
+// Tuesday
+addLecture('Engineering Graphics and Design', 3, 2, 2);
+addLecture('Workshop Practices', 7, 2, 2);
+// Wednesday
+addLecture('Waves, Optics & Quantum Mechanics', 1, 3, 1);
+addLecture('English', 2, 3, 1);
+addLecture('Industrial Sociology', 3, 3, 1);
+addLecture('English', 7, 3, 2);
+// Thursday
+addLecture('Workshop Practices', 1, 4, 1);
+addLecture('Multivariable Calculus', 2, 4, 1);
+addLecture('Waves, Optics & Quantum Mechanics (T)', 3, 4, 1);
+addLecture('Multivariable Calculus (T)', 4, 4, 1);
+addLecture('Workshop Practices', 7, 4, 2);
