9a050dd96254d0dc8d39560d0f137c8764b5c97f
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-04-19 12:13:25 +0530

    Initial commit to add all updated lectures

--- it/lectures.js
+++ it/lectures.js
@@ -7,7 +7,6 @@ addTime('13:00 to 14:00');
 addTime('14:00 to 15:00');
 addTime('15:00 to 16:00');
 addTime('16:00 to 17:00');
-addTime('17:00 to 18:00');
 // Monday
 addLecture('Waves, Optics & Quantum Mechanics', 1, 1, 1);
 addLecture('Multivariable Calculus', 2, 1, 1);
@@ -27,3 +26,11 @@ addLecture('Multivariable Calculus', 2, 4, 1);
 addLecture('Waves, Optics & Quantum Mechanics (T)', 3, 4, 1);
 addLecture('Multivariable Calculus (T)', 4, 4, 1);
 addLecture('Workshop Practices', 7, 4, 2);
+// Friday
+addLecture('Waves, Optics & Quantum Mechanics', 1, 5, 2);
+addLecture('Industrial Sociology', 3, 5, 1);
+addLecture('Multivariable Calculus', 4, 5, 1);
+// Saturday
+addLecture('Waves, Optics & Quantum Mechanics', 1, 6, 1);
+addLecture('English', 2, 6, 1);
+addLecture('Industrial Sociology', 3, 6, 1);
