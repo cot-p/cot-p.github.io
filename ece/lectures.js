0a3c9656d4b2155ba4619e7d7a589555ecef1f8b
cyanide235 <76463243+cyanide235@users.noreply.github.com>
2021-05-01 12:09:44 +0530

    Update lecture.js

--- ece/lectures.js
+++ ece/lectures.js
@@ -1,4 +1,4 @@
-setTitle('Electronics & Communication')
+setTitle('ECE II SEM')
 
 addTime('8:00 to 9:00');
 addTime('9:00 to 10:00');
@@ -9,25 +9,21 @@ addTime('13:00 to 14:00');
 addTime('14:00 to 15:00');
 addTime('15:00 to 16:00');
 addTime('16:00 to 17:00');
-addTime('17:00 to 18:00');
 
-addLecture('Calculus & Differential Equations', 1, 3, 2);
-addLecture('English', 2, 2, 1);
-addLecture('Workshop Practices', 2, 4, 1);
-addLecture('Semiconductor Physics', 2, 5, 1);
-addLecture('English', 2, 6, 1);
-addLecture('Calculus & Differential Equations', 2, 1, 2);
-addLecture('Calculus & Differential Equations', 3, 2, 1);
-addLecture('Semiconductor Physics', 3, 3, 1);
-addLecture('Engineering Graphics & Design', 3, 4, 1);
-addLecture('Engineering Graphics & Design', 4, 1, 2);
-addLecture('Engineering Graphics & Design', 4, 2, 2);
-addLecture('Semiconductor Physics', 4, 6, 1);
-addLecture('Semiconductor Physics Tutorial', 7, 4, 1);
-addLecture('Semiconductor Physics Practical', 8, 2, 2);
-addLecture('Workshop Practices', 8, 3, 2);
-addLecture('English Practical', 8, 4, 2);
-addLecture('Workshop Practices', 8, 5, 2);
-addLecture('Environmental Engineering And Disasters', 10, 3, 1);
-addLecture('Environmental Engineering And Disasters', 10, 4, 1);
-addLecture('Environmental Engineering And Disasters', 10, 5, 1);
+addLecture('Linear Algebra', 1, 1, 1);
+addLecture('Linear Algebra', 1, 2, 1);
+addLecture('Linear Algebra', 1, 3, 1);
+addLecture('Industrial Sociology', 2, 1, 1);
+addLecture('Chemistry', 2, 2, 1);
+addLecture('Chemistry', 2, 3, 1);
+addLecture('Industrial Sociology', 2, 4, 1);
+addLecture('Industrial Sociology', 2, 5, 1);
+addLecture('Chemistry LAB', 3, 2, 3);
+addLecture('Basic EE Lab', 3, 3, 2);
+addLecture('Math Tutorial', 3, 4, 1);
+addLecture('Programming for problem solving', 3, 6, 1);
+addLecture('Basic EE Lecture', 4, 1, 1);
+addLecture('Basic EE Lecture', 4, 4, 1);
+addLecture('Chemistry', 4, 6, 1);
+addLecture('Programming for problem solving', 8, 1, 2);
+addLecture('Basic EE Tutorial', 8, 2, 1);
